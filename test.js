fdd15bb22a3ee125d08850abd0115ff140adc468
Nam <nam085924@gmail.com>
2025-11-25 21:40:34 +0700

    tinh nang dang ky

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 const login = (username, password) => {
   console.log("Login thanh cong");
 };
+
+const register = (username, password, confirmPassword) => {
+  console.log("Dang ky thanh cong");
+};
