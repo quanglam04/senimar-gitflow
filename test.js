d4690a57abb759f901ff3915124d9869a1ce2ab8
Nguyễn Văn Nam <nam085924@gmail.com>
2025-11-25 21:51:48 +0700

    Merge pull request #9 from quanglam04/feature/login
    tinh nang dang ky

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 const login = (username, password) => {
   console.log("Login thanh cong");
 };
+
+const register = (username, password, confirmPassword) => {
+  console.error("Dang ky thanh cong");
+};
+
+login();
+register();
