ae37c5cdf4e4fc550e6374eb3209af8ce953e037
Nam <nam085924@gmail.com>
2025-11-26 20:43:51 +0700

    add

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,7 +3,7 @@ const login = (username, password) => {
     `thong tin username: ${username} thong tin password: ${password}`
   );
   console.log(
-    "login thanh cong. He thong se chuyen huong den trang chu cua ung dung"
+    "login thanh cong. He thong se chuyen huong den trang chu cua ung dungssss"
   );
 };
 
