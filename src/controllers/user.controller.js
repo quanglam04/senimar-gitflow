8c320a41289d8c0d1e2fbd9555bda072e36c8f87
Nguyễn Văn Nam <nam085924@gmail.com>
2025-11-27 16:10:25 +0700

    Fix validate (#17)
    * fix: validate success (#16)
    
    * fix: fix review complete (#16)

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+const validate = (username, password, confirmPassword) => {
+  console.log(
+    "username, password dang ky thanh cong. He thong tu dong chuyen ve trang dang nhap"
+  );
+  console.log("fix thanh cong");
+};
+
+export { validate };
