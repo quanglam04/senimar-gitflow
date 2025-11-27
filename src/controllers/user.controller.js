65866ea1584ba0fc77da20a57e8c9082a61e21b7
Nam <nam085924@gmail.com>
2025-11-27 16:32:52 +0700

    fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,13 @@
 import { register } from "../routes/user.route.js";
 
-const validate = (username, password, confirmPassword) => {
+const validate = (username, password, confirmPassword, a, c, b) => {
   console.log(
     "username, password dang ky thanh cong, thong tin cua user la abcs. He thong tu dong chuyen ve trang dang nhap"
   );
+  console.log("fix thanh cong");
+  for (let i = 0; i < 4; ++i) {
+    console.log(i);
+  }
   console.log("fix thanh cong");
 };
 
