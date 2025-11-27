4779e8514e3e04a3447aae6037ecbac88e442eec
Nam <nam085924@gmail.com>
2025-11-27 16:07:34 +0700

    fix: fix review complete (#16)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const validate = (username, password, confirmPassword) => {
-  console.log("username, password dang ky thanh cong");
+  console.log(
+    "username, password dang ky thanh cong. He thong tu dong chuyen ve trang dang nhap"
+  );
   console.log("fix thanh cong");
 };
 
