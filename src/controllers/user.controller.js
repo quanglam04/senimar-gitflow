d1e88cc2b17be60dd60ddb09bd4bcb29af1fc495
Nguyễn Văn Nam <nam085924@gmail.com>
2025-11-27 16:25:11 +0700

    register (#18)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,12 @@
+import { register } from "../routes/user.route.js";
+
 const validate = (username, password, confirmPassword) => {
   console.log(
-    "username, password dang ky thanh cong. He thong tu dong chuyen ve trang dang nhap"
+    "username, password dang ky thanh cong, thong tin cua user la abcs. He thong tu dong chuyen ve trang dang nhap"
   );
   console.log("fix thanh cong");
 };
+
+register("trinhlam", "123", "123");
 
 export { validate };
