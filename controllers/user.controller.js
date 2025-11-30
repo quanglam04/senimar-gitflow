42f6d57b5fceb497ecdf144af21f141d86c435dd
Huy <snownguyen149@gmail.com>
2025-11-30 10:15:57 +0700

    fix review complete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,5 +2,5 @@ const validate = (username, password) => {};
 
 const login = (username, password) => {
   validate(username, password);
-  console.log("login thanh cong");
+  console.log("login thanh cong. Dieu huong ve trang chu");
 };
